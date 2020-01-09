a65105f3681d4ca8bbb051505c68c95420647db3
tvtu99 <58322507+tvtu99@users.noreply.github.com>
2020-01-09 18:51:44 +0700

    update sinh vien lại commit trước lỗi @@

--- php/sinhvien/sinhvien.php
+++ php/sinhvien/sinhvien.php
@@ -1,6 +1,10 @@
 <?php
-  require_once("../includes/ss-sv.php");
-  include("../includes/head.php");
+require_once("../includes/ss-sv.php");
+include("../includes/head.php");
+include("../includes/conn.php");
+
+mysqli_close($dbcon);
+
 ?>
     <!-- main -->
     <main>
@@ -17,21 +21,31 @@
                 
             </div>
             <!-- main-right -->
-            <div class="main-r col-md-9">
-                                
+            <div class="main-r col-md-9">               
                 <div class="daumuc">
-                    <h3 style="text-align: center;">Tra Cuu diem</h3>
+                    <h3 style="text-align: center;">Tra Cứu Điểm</h3>
                 </div>
                 <form class="frm" action="" method="post">
                     <div class="row row-qlmh">    
-                        
+                        <div class="col-md-12">
+                            <div class="label">Nhập Mã Sinh Viên</div>
+                            <div class="value"><input type="text" value = "175A0" name="svma" size="15" maxlength="8" required></div>
+                        </div>
                         <div class="col-md-12">
                             <div class="label">Năm Học</div>
                             <div class="value">
-                                <select name="level" id="">
-                                    <option>2019</option>
-                                    <option>2020</option>
-                                    <option>2021</option>							
+                                <select name="svnh" id="">
+                                <?php
+                                    $output ='';
+                                    include("../includes/conn.php");
+                                    $sql = "select Nam from giaidoan group by Nam";
+                                    $kq = mysqli_query($dbcon,$sql);
+                                    while($row = mysqli_fetch_assoc($kq))
+                                    {
+                                        $output .= '<option value="'.$row['Nam'].'">'.$row['Nam'].'</option>';
+                                    }
+                                    echo $output;
+                                ?>							
                                 </select>
                             </div>
                     
@@ -39,9 +53,18 @@
                         <div class="col-md-12">
                             <div class="label">Học Kì</div>
                             <div class="value">
-                                <select name="level" id="">
-                                    <option>1</option>
-                                    <option>2</option>							
+                                <select name="svkh" id="">
+                                <?php
+                                    $output ='';
+                                    include("../includes/conn.php");
+                                    $sql = "select KyH from giaidoan group by KyH";
+                                    $kq = mysqli_query($dbcon,$sql);
+                                    while($row = mysqli_fetch_assoc($kq))
+                                    {
+                                        $output .= '<option value="'.$row['KyH'].'">'.$row['KyH'].'</option>';
+                                    }
+                                    echo $output;
+                                ?>										
                                 </select>
                             </div>
                     
@@ -49,11 +72,18 @@
                         <div class="col-md-12">
                             <div class="label">Giai Đoạn</div>
                             <div class="value">
-                                <select name="level" id="">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>							
+                                <select name="svgd" id="">
+                                <?php
+                                    $output ='';
+                                    include("../includes/conn.php");
+                                    $sql = "select GD from giaidoan group by GD";
+                                    $kq = mysqli_query($dbcon,$sql);
+                                    while($row = mysqli_fetch_assoc($kq))
+                                    {
+                                        $output .= '<option value="'.$row['GD'].'">'.$row['GD'].'</option>';
+                                    }
+                                    echo $output;
+                                ?>									
                                 </select>
                             </div>
                     
@@ -68,47 +98,39 @@
                 
             <div class="bangdulieu">
                 <table class="table-data" bgcolor="#FFFFFF">
-                    <tr class="row-first">
-                        <td width="150">Mã Sinh Viên</td>
-                        <td width="100">Giai Đoạn</td>
-                        <td width="150">Mã Môn Học</td>
-                        <td width="150">Quá Trình</td>													
-                        <td width="150">Điểm Thi</td>
-                        <td width="70">Điểm</td>
-                    </tr>
-                    <tr>
-                    <td>175A0</td>
-                          <td>GD12019</td>
-                          <td>CNW</td>
-                          <td>7.0</td>
-                          <td>7.0</td>
-                          <td>7.0</td>
-                    </tr>
-                    <tr>
-                        <td>175A0</td>
-                        <td>GD12019</td>
-                        <td>CNW</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                    </tr>
-                    <tr>
-                        <td>175A0</td>
-                        <td>GD12019</td>
-                        <td>CNW</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                    </tr>
-                    <tr>
-                        <td>175A0</td>
-                        <td>GD12019</td>
-                        <td>CNW</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                        <td>7.0</td>
-                    </tr>
-                    
+                    <?php
+                    if(!$dbcon)
+                    {
+                        die('loi ket loi :' .mysqli_connect_error()); //bao loi
+                    }
+                    else
+                    {   
+                        
+                        if(isset($_POST['svnh'])){
+                            $masv = $_POST['svma'];
+                            $namhoc = $_POST['svnh'];
+                            $kihoc = $_POST['svkh'];
+                            $giaidoan = $_POST['svgd'];
+                            $output = ''; //khai bao chuoi de echo xuat du lieu
+                            $query = "SELECT diem.MaSV,giaidoan.MaGD,monh.TenMH,diem.DQT,diem.DT,diem.diemtongket  FROM giaidoan,lophp,monh,diem,dsl WHERE giaidoan.MaGD = lophp.MaGD AND lophp.MaLHP = dsl.MaLHP AND lophp.MaMH = monh.MaMH AND diem.MaMH=monh.MaMH AND giaidoan.Nam = $namhoc AND giaidoan.KyH = $kihoc AND giaidoan.gd = $giaidoan AND diem.MaSV = (SELECT sv.MaSV FROM sv WHERE sv.MaSV = '$masv' AND dsl.MaSV ='$masv')";
+                            $kq = mysqli_query($dbcon,$query); //truyen sql vao mysql
+                            while($row = mysqli_fetch_array($kq) ) //ham tra ve tat ca ket qua
+                            {
+                                $output .= '<tr><td>' .$row[0]. '</td><td>' .$row[1]. '</td><td>'.$row[2].'</td><td>'.$row[3].'</td><td>'.$row[4].'</td><td>'.$row[5].'</td></tr>';
+                            }
+                            echo '<tr class="row-first">
+                            <td width="150">Mã Sinh Viên</td>
+                            <td width="100">Giai Đoạn</td>
+                            <td width="150">Tên Môn Học</td>
+                            <td width="150">Quá Trình</td>													
+                            <td width="150">Điểm Thi</td>
+                            <td width="70">Điểm</td>
+                            </tr>'.$output;
+                        }
+                    }
+                    //dong kn
+                    ?>
+                   
                 </table>
                 
                 </div>
